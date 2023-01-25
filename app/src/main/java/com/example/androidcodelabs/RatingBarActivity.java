a4e710906cc30a30cb1acf93118bab09f85f25b8
Harsha <shreeharsha.a@improve10x.com>
2023-01-25 16:01:43 +0530

    Created a RatingBarActivity and implemented a ratingBar.

---
+++ app/src/main/java/com/example/androidcodelabs/RatingBarActivity.java
@@ -0,0 +1,24 @@
+package com.example.androidcodelabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.androidcodelabs.databinding.ActivityRatingBarBinding;
+
+public class RatingBarActivity extends AppCompatActivity {
+
+    public ActivityRatingBarBinding ratingBarBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ratingBarBinding = ActivityRatingBarBinding.inflate(getLayoutInflater());
+        setContentView(ratingBarBinding.getRoot());
+        ratingBarBinding.submitbtn.setOnClickListener(v -> {
+            String rating = String.valueOf(ratingBarBinding.ratingBarRb.getRating());
+            Toast.makeText(getApplicationContext(),rating, Toast.LENGTH_SHORT).show();
+        });
+    }
+}
