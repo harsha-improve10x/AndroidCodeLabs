faba51c979a46eb36f0687eb4c9dacc5fb597a72
Harsha <shreeharsha.a@improve10x.com>
2023-01-25 17:05:36 +0530

    Created a DatePickerActivity and implemented a datePicker.

---
+++ app/src/main/java/com/example/androidcodelabs/DatePickerActivity.java
@@ -0,0 +1,36 @@
+package com.example.androidcodelabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.androidcodelabs.databinding.ActivityDatePickerBinding;
+
+public class DatePickerActivity extends AppCompatActivity {
+
+    private ActivityDatePickerBinding datePickerBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        datePickerBinding = ActivityDatePickerBinding.inflate(getLayoutInflater());
+        setContentView(datePickerBinding.getRoot());
+        getSupportActionBar().setTitle("Date Picker");
+        datePickerBinding.textView1.setText("Change Date: " + getCurrentDate());
+        datePickerBinding.button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePickerBinding.textView1.setText("Change Date: " + getCurrentDate());
+            }
+        });
+    }
+
+    private String getCurrentDate() {
+        StringBuilder builder = new StringBuilder();
+        builder.append((datePickerBinding.datePicker.getMonth() + 1) + "/");
+        builder.append(datePickerBinding.datePicker.getDayOfMonth() + "/");
+        builder.append(datePickerBinding.datePicker.getYear());
+        return builder.toString();
+    }
+}
