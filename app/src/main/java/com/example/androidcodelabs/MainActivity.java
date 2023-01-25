46ebcbf12d51e84f56e812ac76bd25de9c1489a4
Harsha <shreeharsha.a@improve10x.com>
2023-01-25 14:57:18 +0530

    Displayed a Toast Message

--- app/src/main/java/com/example/androidcodelabs/MainActivity.java
+++ app/src/main/java/com/example/androidcodelabs/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.androidcodelabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Hi, Welcome To Improve10X", Toast.LENGTH_SHORT).show();
     }
 }
